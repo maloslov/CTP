a788fdf2ba5f81384677991df4f844f8101ee68f
Alex Basov <Basow@yandex.ru>
2020-03-21 04:50:47 +0300

    start lab3

---
+++ src/lab3/JImageDisplay.java
@@ -0,0 +1,29 @@
+package lab3;
+
+import javax.swing.JComponent;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+
+public class JImageDisplay extends JComponent {
+
+    private BufferedImage bufIm;
+
+    public JImageDisplay(int width, int height){
+        bufIm = new BufferedImage(width, height, TYPE_INT_RGB);
+        setPreferredSize(new Dimension(width,height));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //g.drawImage(image, 0, 0, image.getWidth, image.getHeight, null);
+    }
+    public void clearImage(){
+
+    }
+    public void drawPixel(int x, int y, int rgbColor){
+
+    }
+}
