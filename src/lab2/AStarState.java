07fe89968789dadb328db75cfff55ce9dafcfd85
Alex Basov <Basow@yandex.ru>
2020-03-21 03:32:08 +0300

    filled 5 methods in AStarState + few RU comms

--- src/lab2/AStarState.java
+++ src/lab2/AStarState.java
@@ -1,19 +1,36 @@
 package lab2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
 /**
  * This class stores the basic state necessary for the A* algorithm to compute a
  * path across a map.  This state includes a collection of "open waypoints" and
  * another collection of "closed waypoints."  In addition, this class provides
  * the basic operations that the A* pathfinding algorithm needs to perform its
  * processing.
+ *
+ * Этот класс содержит основное состояние, необходимое алгоритму А* для подсчета
+ * пути по карте. Это состояние включает набор "открытых вершин" и другой
+ * набор "закрытых вершин". Вдобавок, этот класс обеспечивает основные
+ * операции, необходимые поисковому алгоритму А* для выполнения своей обработки.
  **/
 public class AStarState
 {
-    /** This is a reference to the map that the A* algorithm is navigating. **/
+    /** This is a reference to the map that the A* algorithm is navigating.
+     *
+     * Это ссылка на карту, которая наводит алгоритм А*.
+     **/
     private Map2D map;
 
+    private HashMap<Location,Waypoint> openVerticles, closedVerticles;
 
     /**
      * Initialize a new state object for the A* pathfinding algorithm to use.
+     *
+     * Инициализация нового состояния объекта для поисковика пути алгоритма А*.
      **/
     public AStarState(Map2D map)
     {
@@ -21,22 +38,49 @@ public class AStarState
             throw new NullPointerException("map cannot be null");
 
         this.map = map;
+        openVerticles = new HashMap<>();
+        closedVerticles = new HashMap<>();
     }
 
-    /** Returns the map that the A* pathfinder is navigating. **/
+    /** Returns the map that the A* pathfinder is navigating.
+     *
+     * Возвращает карту для навигации поисковика пути алгоритма А*.
+     **/
     public Map2D getMap()
     {
         return map;
+    }
+
+    /** Returns the current number of open waypoints.
+     *
+     * Возвращает текущее число открытых вершин
+     **/
+    public int numOpenWaypoints()
+    {
+        /** Done **/
+        return openVerticles.size();
     }
 
     /**
      * This method scans through all open waypoints, and returns the waypoint
      * with the minimum total cost.  If there are no open waypoints, this method
      * returns <code>null</code>.
+     *
+     * Этот метод сканирует все открытые вершины и возвращает вершину
+     * с наименьшей общей стоимостью. Иначе возвращает нуль.
      **/
     public Waypoint getMinOpenWaypoint()
     {
-        // TODO:  Implement.
+        /** DONE: Implement. **/
+        if (!openVerticles.isEmpty()) {
+            ArrayList<Waypoint> w = new ArrayList<>(openVerticles.values());
+            Waypoint minCost = w.get(0);
+            for (int i = 0; i < w.size(); i++) {
+                if (minCost.getTotalCost() > w.get(i).getTotalCost())
+                    minCost = w.get(i);
+            }
+            return minCost;
+        }
         return null;
     }
 
@@ -48,39 +92,47 @@ public class AStarState
      * new waypoint's location, the new waypoint replaces the old one <em>only
      * if</em> the new waypoint's "previous cost" value is less than the current
      * waypoint's "previous cost" value.
+     *
+     * Этот метод добавляет вершины (или возможные обновления вершины) в набор
+     * открытых вершин. Если
      **/
     public boolean addOpenWaypoint(Waypoint newWP)
     {
-        // TODO:  Implement.
+        /** DONE:  Implement. **/
+        if (!openVerticles.containsKey(newWP.loc) || openVerticles.get(newWP.loc).getPreviousCost() > newWP.getPreviousCost()) {
+            openVerticles.put(newWP.loc,newWP);
+        }
         return false;
-    }
-
-
-    /** Returns the current number of open waypoints. **/
-    public int numOpenWaypoints()
-    {
-        // TODO:  Implement.
-        return 0;
-    }
-
-
-    /**
-     * This method moves the waypoint at the specified location from the
-     * open list to the closed list.
-     **/
-    public void closeWaypoint(Location loc)
-    {
-        // TODO:  Implement.
     }
 
     /**
      * Returns true if the collection of closed waypoints contains a waypoint
      * for the specified location.
+     *
+     * Возвращает 1 если набор закрытых вершин содержит вершину
+     * для заданного местоположения.
      **/
     public boolean isLocationClosed(Location loc)
     {
-        // TODO:  Implement.
+        /** DONE: Implement. **/
+        if(closedVerticles.containsKey(loc))
+            return true;
         return false;
     }
+
+    /**
+     * This method moves the waypoint at the specified location from the
+     * open list to the closed list.
+     *
+     * Этот метод передвигают вершину на заданное место из списка
+     * открытых вершин в список закрытых.
+     **/
+    public void closeWaypoint(Location loc)
+    {
+        /** DONE: Implement. **/
+        closedVerticles.put(loc,openVerticles.get(loc));
+        openVerticles.remove(loc);
+    }
+
 }
 
--- src/lab2/Location.java
+++ src/lab2/Location.java
@@ -1,4 +1,7 @@
 package lab2;
+
+import java.util.Objects;
+
 /**
  * This class represents a specific location in a 2D map.  Coordinates are
  * integer values.
@@ -11,6 +14,19 @@ public class Location
     /** Y coordinate of this location. **/
     public int yCoord;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return xCoord == location.xCoord &&
+                yCoord == location.yCoord;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoord, yCoord);
+    }
 
     /** Creates a new location with the specified integer coordinates. **/
     public Location(int x, int y)
