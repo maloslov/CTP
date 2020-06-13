40e17fbaeaaec69230c140657e41919381929c9f
Alex Basov <Basowf@yandex.ru>
2020-06-13 11:02:16 +0300

    Lab7.

--- src/lab7/Crawler.java
+++ src/lab7/Crawler.java
@@ -1,152 +1,73 @@
 package lab7;
 
 import java.net.*;
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Crawler {
-    public static void main(String[] arg) {
-        String[] args = new String[]{"http://www.wikidot.com/","4"};
-        int depth = 0;
+    private int maxDepth;
+    private LinkedList<URLDepthPair> pending = new LinkedList<>();
+    private LinkedList<URLDepthPair> processed = new LinkedList<>();
 
-        if (args.length != 2) {
-            System.out.println("Wrong args! use: java Crawler <URL> <depth>");
-            System.exit(1);
-        }
-        else {
-            try {
-                depth = Integer.parseInt(args[1]);
-            }
-            catch (NumberFormatException nfe) {
-                System.out.println("Wrong args! use: java Crawler <URL> <depth>");
-                System.exit(1);
-            }
-        }
+    public Crawler(String startLink, int maxDepth) {
+        this.maxDepth = maxDepth;
+        pending.add(new URLDepthPair(startLink, 0));
+    }
 
-        LinkedList<URLDepthPair> processedURLs = new LinkedList<URLDepthPair>(),
-                pendingURLs = new LinkedList<URLDepthPair>();
-
-        URLDepthPair currentDepthPair = new URLDepthPair(args[0], 0);
-        pendingURLs.add(currentDepthPair);
-        ArrayList<String> seenURLs = new ArrayList<String>();
-        seenURLs.add(currentDepthPair.getURL());
-
-        while (pendingURLs.size() != 0) {
-            URLDepthPair depthPair = pendingURLs.pop();
-            processedURLs.add(depthPair);
-
-            int curDepth = depthPair.getDepth();
-            LinkedList<String> linksList = new LinkedList<String>();
-            linksList = Crawler.getAllLinks(depthPair);
-
-            if (curDepth < depth) {
-                for (int i = 0; i < linksList.size(); i++) {
-
-                    String newURL = linksList.get(i);
-
-                    if (seenURLs.contains(newURL)) {
-                        continue;
-                    }
-                    else {
-                        URLDepthPair newPair = new URLDepthPair(newURL, curDepth + 1);
-                        pendingURLs.add(newPair);
-                        seenURLs.add(newURL);
-                    }
-                }
-            }
-        }
-        Iterator<URLDepthPair> iter = processedURLs.iterator();
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
+    public void start() throws IOException {
+        while (pending.size() > 0) {
+            process(pending.removeFirst());
         }
     }
 
-    private static LinkedList<String> getAllLinks(URLDepthPair pairs) {
-        Socket sock;
-        OutputStream out;
-        InputStream in;
-        LinkedList<String> URLs = new LinkedList<String>();
-        String line;
-        int beginIndex = 0,
-                endIndex = 0,
-                index = 0;
+    private void process(URLDepthPair pair) throws IOException {
         final String URL_START = "a href=\"",
-                URL_END = "\"";
+                PREFIX = "http";
+        URL url = new URL(pair.getURL());
+        URLConnection urlConnection = url.openConnection();
+        String redirect = urlConnection.getHeaderField("Location"),
+                input,
+                link;
 
-        try {
-            sock = new Socket(pairs.getWebHost(), 80);
+        if (redirect != null) {
+            urlConnection = new URL(redirect).openConnection();
         }
-        catch (UnknownHostException e) {
-            //    System.err.println("UnknownHostException1: " + e.getMessage());
-            return URLs;
-        }
-        catch (IOException ee) {
-            //    System.err.println("IOException2: " + ee.getMessage());
-            return URLs;
-        }
-        try {
-            sock.setSoTimeout(3000);
-        }
-        catch (SocketException eee) {
-            //    System.err.println("SocketException3: " + eee.getMessage());
-            return URLs;
-        }
+        processed.add(pair);
+        System.out.println(pair.toString());
 
-        String webHost = pairs.getWebHost(),
-                docPath = pairs.getDocPath();
-
-        try {
-            out = sock.getOutputStream();
+        if (pair.getDepth() == maxDepth) {
+            return;
         }
-        catch (IOException eeee) {
-            //    System.err.println("IOException4: " + eeee.getMessage());
-            return URLs;
-        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
-        PrintWriter myWriter = new PrintWriter(out, true);
-        myWriter.println("GET " + docPath + " HTTP/1.1");
-        myWriter.println("Host: " + webHost);
-        myWriter.println("Connection: close");
-        myWriter.println();
-
-        try {
-            in = sock.getInputStream();
-        }
-        catch (IOException eeeee){
-            //   System.err.println("IOException5: " + eeeee.getMessage());
-            return URLs;
-        }
-        BufferedReader BuffReader = new BufferedReader(new InputStreamReader(in));
-
-        // Try to read
-        while (true) {
-            try {
-                line = BuffReader.readLine();
-            }
-            catch (IOException except) {
-                //    System.err.println("IOException6: " + except.getMessage());
-                return URLs;
-            }
-            if (line == null)
-                break;
-
-            while (true) {
-                index = line.indexOf(URL_START, index);
-                if (index == -1)
-                    break;
-
-                index += URL_START.length();
-                beginIndex = index;
-                endIndex = line.indexOf(URL_END, index);
-                index = endIndex;
-                String newLine = "";
-                try{
-                    newLine = line.substring(beginIndex, endIndex);
-                    URLs.add(newLine);
-                } catch(StringIndexOutOfBoundsException ignored) { break; }
+        while ((input = reader.readLine()) != null) {
+            while (input.contains(URL_START + PREFIX)) {
+                input = input.substring(input.indexOf(URL_START + PREFIX) + 8);
+                link = input.substring(0, input.indexOf('\"'));
+                if(link.contains(" ")) {
+                    link = link.replace(" ", "%20");
+                }
+                // if same links
+                if (pending.contains(new URLDepthPair(link, 0)) ||
+                        processed.contains(new URLDepthPair(link, 0))) {
+                    continue;
+                }
+                pending.add(new URLDepthPair(link, pair.getDepth() + 1));
             }
         }
-        return URLs;
+        reader.close();
     }
+
+    /*
+    public String getAllLinks() {
+        String res = "";
+        for (URLDepthPair elem : processed){
+            res += elem.toString() + "\r\n";
+        }
+        res += "Number of links: " + processed.size();
+        return res;
+    }
+    */
 }
+
 
---
+++ src/lab7/CrawlerApp.java
@@ -0,0 +1,13 @@
+package lab7;
+
+import java.io.IOException;
+
+public class CrawlerApp {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 2){
+            Crawler crawler = new Crawler(args[0], Integer.parseInt(args[1]));
+            crawler.start();
+        } else
+            System.out.println("Wrong args! usage: <start URL> <depth>");
+    }
+}
